9d264ecf332aa0269a7a3583dac1e56dad4a73c3
stjuks <stjuks@ttu.ee>
2020-03-16 12:36:00 +0200

    Transaction list loading optimization

--- packages/ui/src/components/Transactions/index.tsx
+++ packages/ui/src/components/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useMemo } from 'react';
 import { observer } from 'mobx-react-lite';
 import moment from 'moment';
 import currency from 'currency.js';
@@ -34,11 +34,15 @@ interface TransactionsConfig {
 const Transactions: React.FC<TransactionsProps> = observer(({ type }) => {
   const transactionStore = useContext(TransactionStoreContext);
 
-  const [filter, setFilter] = useState<TransactionQueryInput>({
-    startDate: new Date('2020-01-01'),
-    endDate: new Date('2020-12-31'),
-    generalQuery: ''
-  });
+  const [startDate, setStartDate] = useState(new Date('2020-01-01'));
+  const [endDate, setEndDate] = useState(new Date('2020-12-31'));
+  const [generalQuery, setGeneralQuery] = useState('');
+
+  const filter = useMemo(() => ({ startDate, endDate, generalQuery }), [
+    startDate,
+    endDate,
+    generalQuery
+  ]);
 
   const config: TransactionsConfig =
     type === 'EXPENSE'
@@ -59,12 +63,7 @@ const Transactions: React.FC<TransactionsProps> = observer(({ type }) => {
           hasNextPage: transactionStore.paginatedIncomes.pageInfo.hasNextPage
         };
 
-  const headerIcons = [
-    <HeaderSearch
-      onChange={query => setFilter({ ...filter, generalQuery: query })}
-      placeholder="Otsi kulu"
-    />
-  ];
+  const headerIcons = [<HeaderSearch onChange={setGeneralQuery} placeholder="Otsi kulu" />];
 
   useEffect(() => {
     config.fetchTransactions(filter);
@@ -74,19 +73,9 @@ const Transactions: React.FC<TransactionsProps> = observer(({ type }) => {
     <>
       <Header title={config.headerTitle} components={headerIcons} />
       <SortingContainer>
-        <MenuDateInput
-          noFormik
-          name="startDate"
-          onChange={value => setFilter({ ...filter, startDate: value })}
-          value={filter.startDate}
-        />
+        <MenuDateInput noFormik name="startDate" onChange={setStartDate} value={filter.startDate} />
         -
-        <MenuDateInput
-          noFormik
-          name="endDate"
-          onChange={value => setFilter({ ...filter, endDate: value })}
-          value={filter.endDate}
-        />
+        <MenuDateInput noFormik name="endDate" onChange={setEndDate} value={filter.endDate} />
       </SortingContainer>
       <ContentContainer>
         {config.transactions.map(transaction => (
